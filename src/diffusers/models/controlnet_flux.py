8fcd27c29eac921a120c6f3fd5713921864c40c3
wangqixun <253817124@qq.com>
2024-08-14 16:34:33 +0800

    support union controlnet

--- src/diffusers/models/controlnet_flux.py
+++ src/diffusers/models/controlnet_flux.py
@@ -22,7 +22,7 @@ from ..configuration_utils import ConfigMixin, register_to_config
 from ..loaders import PeftAdapterMixin
 from ..models.attention_processor import AttentionProcessor
 from ..models.modeling_utils import ModelMixin
-from ..models.normalization import RMSNorm, AdaLayerNormContinuous
+from ..models.normalization import AdaLayerNormContinuous
 from ..utils import USE_PEFT_BACKEND, is_torch_version, logging, scale_lora_layers, unscale_lora_layers
 from .controlnet import BaseOutput, zero_module
 from .embeddings import CombinedTimestepGuidanceTextProjEmbeddings, CombinedTimestepTextProjEmbeddings
