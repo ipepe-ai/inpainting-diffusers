909d81223afd594c83e9cf767a29e705b519590f
Gothos <vishnujaddipal@gmail.com>
2024-08-15 07:07:14 +0000

    add cn-inpaint/cn-img2img

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -125,6 +125,8 @@ else:
         "AnimateDiffVideoToVideoPipeline",
     ]
     _import_structure["flux"] = [
+        "FluxControlNetInpaintPipeline",
+        "FluxControlNetImg2ImgPipeline",
         "FluxControlNetPipeline",
         "FluxImg2ImgPipeline",
         "FluxInpaintPipeline",
@@ -498,7 +500,14 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
             VersatileDiffusionTextToImagePipeline,
             VQDiffusionPipeline,
         )
-        from .flux import FluxControlNetPipeline, FluxImg2ImgPipeline, FluxInpaintPipeline, FluxPipeline
+        from .flux import (
+            FluxControlNetImg2ImgPipeline,
+            FluxControlNetInpaintPipeline,
+            FluxControlNetPipeline,
+            FluxImg2ImgPipeline,
+            FluxInpaintPipeline,
+            FluxPipeline,
+        )
         from .hunyuandit import HunyuanDiTPipeline
         from .i2vgen_xl import I2VGenXLPipeline
         from .kandinsky import (
